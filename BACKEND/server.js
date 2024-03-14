59f222d64f61b5a38bdd83b552a1f4a773e430fe
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2024-03-15 02:45:35 +0530

    feat: Add new dependencies and files, update existing ones
    This commit adds new dependencies to the backend, including nodemon and various other packages. It also includes new files and updates the server.js file to incorporate these changes.

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,11 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require("dotenv");
 const app = express();
-const cors = require('cors');
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8070;
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB connection success!");
+}).catch((error) => {
+    console.log("MongoDB connection error:", error);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number: ${PORT}`);
+});
 
