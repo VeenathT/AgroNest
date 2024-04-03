60d0590d316689acd76a18dab5d2673a7694852e
VeenathT <veenaththamuditha@gmail.com>
2024-04-03 22:13:39 +0530

    add fertilizer suggestions codes and update the serverjs code

--- BACKEND_Veenath/server.js
+++ BACKEND_Veenath/server.js
@@ -22,18 +22,22 @@ mongoose.connect(URL,{
 
 const connection = mongoose.connection;
 connection.once("open",() => {
-    console.log("Mongodb Connection Success!");
+    console.log("MongoDB Connection Success!");
 });
 
 // Import farmer feedback routes
 const farmerFeedbackRoutes = require("./routes/farmerfeedbacks");
 // Import farmer report routes
 const farmerReportRoutes = require("./routes/farmerReports");
+// Import fertilizer suggestion routes
+const fertilizerSuggestionRoutes = require("./routes/fertilizerSuggestions");
 
 // Use farmer feedback routes
 app.use("/api/feedbacks", farmerFeedbackRoutes);
 // Use farmer report routes
 app.use("/api/reports", farmerReportRoutes);
+// Use fertilizer suggestion routes
+app.use("/api/suggestions", fertilizerSuggestionRoutes);
 
 app.listen(PORT, () =>{
     console.log(`Server is up and running on port number: ${PORT}`);
