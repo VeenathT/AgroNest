eb51e48fa8a5a0c4663bf480cd51db76c4b168ee
Thisaravi <hashinikathisaravi0523@gmail.com>
2024-04-09 14:19:31 +0530

    add sign up page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' elememt={<App/>} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+
 );
 
 reportWebVitals();
