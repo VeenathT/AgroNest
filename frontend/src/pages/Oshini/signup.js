3c024981b2f4981589c0174c68039eb8b14790a4
Oshini Sandunika <oshinisandunika416@gmail.com>
2024-04-19 20:03:27 +0530

    validations added to signup

--- frontend/src/pages/Oshini/signup.js
+++ frontend/src/pages/Oshini/signup.js
@@ -42,133 +42,149 @@ const SignupForm = () => {
   });
 
   const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [phoneError, setPhoneError] = useState('');
+  const [usernameError, setUsernameError] = useState('');
 
-  const handleChange = (e) => {
+  const handleChange = async (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
 
-    
+    if (e.target.name === 'phone') {
+      // Validate phone number
+      if (!/^[0-9]{10}$/.test(e.target.value)) {
+        setPhoneError('Invalid phone number');
+      } else {
+        setPhoneError('');
+      }
+    }
+
+    if (e.target.name === 'userName') {
+      try {
+        const response = await axios.get('http://localhost:8070/labAccount/checkUserName', { userName: e.target.value });
+        if (response.data.status) {
+          setUsernameError('Username already exists');
+        } else {
+          setUsernameError('');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        alert('Error in fetching usernames.');
+      }
+    }
   };
 
   const handleSignup = async (e) => {
     e.preventDefault(); // Prevent the default form submission behavior
-    if (formData.userName) {
+    if (formData.userName && !phoneError && !usernameError) {
       try {
-        const response = await axios.get('http://localhost:8070/labAccount/checkUserName', { params: { userName: formData.userName } });
-        if (response.data.status) {
-
-          alert('Username already exists');
-        } else {
-          try {
-            const response = await axios.post('http://localhost:8070/labAccount/add', formData);
-            console.log(response.data);
-            alert('Signup Successful!');
-            navigate('/labLogin');
-          } catch (error) {
-            console.error('Error:', error);
-            alert('Signup Failed! Please try again later.');
-          } finally {
-            setButtonDisabled(false); // Re-enable the button after form submission
-          }
-        }
+        const response = await axios.post('http://localhost:8070/labAccount/add', formData);
+        console.log(response.data);
+        alert('Signup Successful!');
+        navigate('/labLogin');
       } catch (error) {
         console.error('Error:', error);
+        alert('Signup Failed! Please try again later.');
+      } finally {
+        setButtonDisabled(false); // Re-enable the button after form submission
       }
     }
   };
   
-  
-
   return (
     <form onSubmit={(e) => handleSignup(e)}>
-    <Container>
-      <FormContainer>
-        <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>Sign Up</h1>
-        <div style={{ marginBottom: '5px' }}>
-          <TextField
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            label="Name"
-            fullWidth
-            variant="outlined"
-          />
-        </div>
-        <div style={{ marginBottom: '5px' }}>
-          <TextField
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-            label="Address"
-            fullWidth
-            variant="outlined"
-          />
-        </div>
-        <div style={{ marginBottom: '5px' }}>
-          <TextField
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            label="Phone"
-            fullWidth
-            variant="outlined"
-          />
-        </div>
-        <div style={{ marginBottom: '5px' }}>
-          <TextField
-            name="district"
-            value={formData.district}
-            onChange={handleChange}
-            label="District"
-            fullWidth
-            variant="outlined"
-          />
-        </div>
-        <div style={{ marginBottom: '5px' }}>
-          <TextField
-            name="city"
-            value={formData.city}
-            onChange={handleChange}
-            label="City"
-            fullWidth
-            variant="outlined"
-          />
-        </div>
-        <div style={{ marginBottom: '5px' }}>
-          <TextField
-            name="userName"
-            value={formData.userName}
-            onChange={handleChange}
-            label="Username"
-            fullWidth
-            variant="outlined"
-          />
-        </div>
-        <div>
-          <TextField
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            label="Password"
-            fullWidth
-            type="password"
-            variant="outlined"
-          />
-        </div>
-        <div style={{ marginTop: '20px' }}>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth
-          >
-            Sign Up
-          </Button>
-        </div>
-      </FormContainer>
-    </Container></form>
+      <Container>
+        <FormContainer>
+          <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>Sign Up</h1>
+          <div style={{ marginBottom: '5px' }}>
+            <TextField
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              label="Name"
+              fullWidth
+              variant="outlined"
+            />
+          </div>
+          <div style={{ marginBottom: '5px' }}>
+            <TextField
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              label="Address"
+              fullWidth
+              variant="outlined"
+            />
+          </div>
+          <div style={{ marginBottom: '5px' }}>
+            <TextField
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              label="Phone"
+              fullWidth
+              variant="outlined"
+              error={!!phoneError}
+              helperText={phoneError}
+            />
+          </div>
+          <div style={{ marginBottom: '5px' }}>
+            <TextField
+              name="district"
+              value={formData.district}
+              onChange={handleChange}
+              label="District"
+              fullWidth
+              variant="outlined"
+            />
+          </div>
+          <div style={{ marginBottom: '5px' }}>
+            <TextField
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+              label="City"
+              fullWidth
+              variant="outlined"
+            />
+          </div>
+          <div style={{ marginBottom: '5px' }}>
+            <TextField
+              name="userName"
+              value={formData.userName}
+              onChange={handleChange}
+              label="Username"
+              fullWidth
+              variant="outlined"
+              error={!!usernameError}
+              helperText={usernameError}
+            />
+          </div>
+          <div>
+            <TextField
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              label="Password"
+              fullWidth
+              type="password"
+              variant="outlined"
+            />
+          </div>
+          <div style={{ marginTop: '20px' }}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Sign Up
+            </Button>
+          </div>
+        </FormContainer>
+      </Container>
+    </form>
   );
 };
 
