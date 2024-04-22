b7188b6cb5dfa450cad6db0fb7506cd10f3ed86d
VeenathT <veenaththamuditha@gmail.com>
2024-04-22 14:24:05 +0530

    issue in navbar by importing nilupul page. (white)

--- frontend/src/pages/common/LandingPage.js
+++ frontend/src/pages/common/LandingPage.js
@@ -3,7 +3,7 @@ import ArticleList from '../Nilupul/ArticleList';
 
 const DealerSignUp = () => {
   return (
-    <div>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
       <ArticleList />
     </div>
   
