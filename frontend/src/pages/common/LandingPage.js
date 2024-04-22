df002f3ce240ef31eb817ea8cfbaefcdeb76a8b9
VeenathT <veenaththamuditha@gmail.com>
2024-04-22 14:01:12 +0530

    remove landing page img

--- frontend/src/pages/common/LandingPage.js
+++ frontend/src/pages/common/LandingPage.js
@@ -4,13 +4,9 @@ import ArticleList from '../Nilupul/ArticleList';
 const DealerSignUp = () => {
   return (
     <div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <img src='./landing.png' style={{ width: '1920px', height: '600px'}} />
-    </div>
-    <div>
       <ArticleList />
     </div>
-    </div>
+  
     
     
   );
