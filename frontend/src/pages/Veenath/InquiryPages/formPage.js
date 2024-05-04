e881bde077f3c14cf395d0fc9567f251e92f97af
VeenathT <veenaththamuditha@gmail.com>
2024-05-05 00:00:38 +0530

    area names

--- frontend/src/pages/Veenath/InquiryPages/formPage.js
+++ frontend/src/pages/Veenath/InquiryPages/formPage.js
@@ -126,11 +126,30 @@ const FormPage = () => {
             <InputLabel>Select Area</InputLabel>
             <Select value={area} onChange={(e) => setArea(e.target.value)} required>
               <MenuItem value="">Select Area</MenuItem>
-              {[...Array(24)].map((_, index) => (
-                <MenuItem key={index} value={`Area ${index + 1}`}>
-                  Area {index + 1}
-                </MenuItem>
-              ))}
+              <MenuItem value="Kandy">Kandy</MenuItem>
+              <MenuItem value="Colombo">Colombo</MenuItem>
+              <MenuItem value="Galle">Galle</MenuItem>
+              <MenuItem value="Jaffna">Jaffna</MenuItem>
+              <MenuItem value="Kurunegala">Kurunegala</MenuItem>
+              <MenuItem value="Anuradhapura">Anuradhapura</MenuItem>
+              <MenuItem value="Gampaha">Gampaha</MenuItem>
+              <MenuItem value="Matara">Matara</MenuItem>
+              <MenuItem value="Hambantota">Hambantota</MenuItem>
+              <MenuItem value="Badulla">Badulla</MenuItem>
+              <MenuItem value="Ratnapura">Ratnapura</MenuItem>
+              <MenuItem value="Ampara">Ampara</MenuItem>
+              <MenuItem value="Monaragala">Monaragala</MenuItem>
+              <MenuItem value="Polonnaruwa">Polonnaruwa</MenuItem>
+              <MenuItem value="Puttalam">Puttalam</MenuItem>
+              <MenuItem value="Kegalle">Kegalle</MenuItem>
+              <MenuItem value="Matale">Matale</MenuItem>
+              <MenuItem value="Nuwara Eliya">Nuwara Eliya</MenuItem>
+              <MenuItem value="Trincomalee">Trincomalee</MenuItem>
+              <MenuItem value="Batticaloa">Batticaloa</MenuItem>
+              <MenuItem value="Mannar">Mannar</MenuItem>
+              <MenuItem value="Vavuniya">Vavuniya</MenuItem>
+              <MenuItem value="Kilinochchi">Kilinochchi</MenuItem>
+              <MenuItem value="Mullaitivu">Mullaitivu</MenuItem>
             </Select>
           </FormControl>
           
