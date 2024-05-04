8dcf07c53567b4f029eab0c5b45260b72a159775
pxrxra <it22918338@my.sliit.lk>
2024-05-04 20:33:27 +0530

    vg

--- frontend/src/Component/layout/Layout.js
+++ frontend/src/Component/layout/Layout.js
@@ -4,10 +4,6 @@ import Footer from '../common/footer'
 import TopBar from '../../pages/common/topbar'
 import Router from '../../router/Router'
 
-import React, { useState, useEffect } from 'react';
-import Header from '../common/header';
-import Footer from '../common/footer';
-import Router from '../../router/Router';
 
 const Layout = () => {
 
@@ -34,29 +30,6 @@ const Layout = () => {
             </div>
           );
   };
-  const [showFooter, setShowFooter] = useState(false);
-  const [lastScrollTop, setLastScrollTop] = useState(0);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      setShowFooter(currentScrollTop > lastScrollTop);
-      setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
-    };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [lastScrollTop]);
-
-  return (
-    <div style={{ paddingBottom: showFooter ? '80px' : '0', paddingTop: '64px' }}>
-      <Header />
-      <div style={{ marginTop: '64px' }}>
-        <Router />
-      </div>
-      <Footer visible={showFooter} />
-    </div>
-  );
-};
-
-export default Layout;
+export default Layout
